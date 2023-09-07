0724ba808015403021a64179b3eaee2477d939fe
Frantz Alexander <mr.frantz.alexander@gmail.com>
2023-09-07 00:50:18 -0400

    added error handling functionality

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ phonetic_dataframe = pd.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row["letter"]: row["code"] for index, row in phonetic_dataframe.iterrows()}
 
-word = input("Enter a word: \n").upper()
-    
-phonetic = [phonetic_dict[letter] for letter in word]
+def create_phonetic():
+    word = input("Enter a word: \n").upper()
+    try:
+        phonetic = [phonetic_dict[letter] for letter in word]
+        
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        create_phonetic()
+    else:
+        print(phonetic)
+
+create_phonetic()
